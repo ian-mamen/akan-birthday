ddb4f45f15e24893a913a6c8111acd190d7a6cdb
Ian Amuga <amuga72@gmail.com>
2020-05-11 00:58:34 +0300

    Added the forms for inputting the date and gender

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-h1 {
-  text-align: center;
-}
+function getAkanName(){
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+    var myBirthday = document.getElementById("myBirthDate").value;
+    var myGender = document.getElementsByName("gender");
+    var dateOfBirth = new Date(myBirthday);
+    var dayOfTheWeek = dateOfBirth.getDay();
+      if (dateOfBirth==='') {
+
+      }
